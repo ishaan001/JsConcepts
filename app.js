950ed5a9b680a1ed7c4882449252cefce4872b10
ishaan001 <ishaanveerdadhwal@gmail.com>
2018-09-12 22:18:12 +0530

    datatype and conversions

---
+++ app.js
@@ -0,0 +1,69 @@
+//alert('Hello');
+// log to console
+console.log('Hello world'); //string
+console.log(123); //number
+console.log(true); // boolean
+var greeting = "hello";
+console.log(greeting); // variables
+console.log([1,2,3,4,5]); // array
+console.log({a:1,b:2}); //objects 
+console.table({a:1,b:2}); //prints data in table in console
+console.error('this is some error'); // somewhat like printing in catch
+console.clear();
+console.warn('this is a warning');
+console.time('hello'); // timer which will count the time to print hello world
+  console.log('Hello world');
+  console.log('Hello world');
+  console.log('Hello world');
+  console.log('Hello world');
+  console.log('Hello world');
+  console.log('Hello world');
+  console.log('Hello world');
+console.timeEnd('hello');
+//single line comments 
+
+/*
+
+multi
+ line
+  comments 
+
+*/
+
+// var name = "Ishaan Veer Dadhwal";
+// console.log(typeof name);
+// console.log(name.length);
+
+// bool to a string
+
+var name =5;
+console.log(name);
+console.log(name.length);
+
+// printing the date
+console.log(new Date());
+
+var arr = String([1,2,3,4]);
+var arr1 = [7,8,9,0];
+console.log(arr1);
+console.log(arr1.length);
+console.log(arr1[0]);
+console.log(arr);
+console.log(arr.length);
+
+
+// playing with numbers 
+
+var val1 = 57.12;
+console.log(val1.toFixed());
+console.log(val1.toFixed(3));
+
+var magicPi = Math.PI;
+console.log(magicPi);
+console.log(magicPi.toFixed(5));
+
+var EulerNo = Math.E;
+console.log(EulerNo);
+console.log(Math.round(2.4));
+console.log(Math.ceil(2.3));
+console.log(Math.floor(2.9));
