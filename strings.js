28111293c7a7444658c4a142d5e131292841913a
ishaan001 <ishaanveerdadhwal@gmail.com>
2018-09-13 01:05:59 +0530

    string usage

---
+++ strings.js
@@ -0,0 +1,9 @@
+var firstname= 'ishaan';
+var middlename= 'veer';
+var lastname= 'dadhwal';
+
+console.log(firstname+ ' ' + middlename+' ' + lastname);
+console.log(firstname.concat("hello",lastname));
+var fullName = firstname+ ' ' + middlename+' ' + lastname;
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
